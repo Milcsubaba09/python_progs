defa74634b6e5c982161705e53c847a1851ba801
Milán <petomilan2009@gmail.com>
2019-01-27 18:29:20 +0100

    Kérdésekkel fejlesztve, üres sorok kitörölve, "sys.exit" hozzáadva.

--- robot.py
+++ robot.py
@@ -1,3 +1,4 @@
+import sys
 print ("*="*40)
 
 while True:
@@ -24,11 +25,44 @@ print ("Szia",a,",",
         "Te", b, "éves vagy",",",
         "És a(z)",c,"iskolába jársz."
 )
+print ("")
+x = False
 
+while x != True:
+    j = input("Melyik a kedvenc számod: ")
+    x = j.isdigit()
+print ("")
+while True:
+    try:
+        y = input("Írd be hogy fiú vagy lány esetleg robot vagy:")
+        z = float(y)
+    except ValueError:
+        break
+print ("")
+print (
+    "A kedvenc számod a(z)",j,","
+    "És", y ,"vagy."
+)
+print ("")
+v = False
+while v != True:
+    w = input("Írd be számmal hogy mikor születtél:")
+    if len(w) == 0:
+        print("Write somethings")
+        v = False
+    else:
+        pass
+        try:
+            int(w)
+            v = False
+        except:
+            try:
+                float(w)
+                v = True
+            except:
+                    pass
 
+print ("")
+print ("Te",w,"születtél.")
 
-
-
-
-
-
+sys.exit(0)
